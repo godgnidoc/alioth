54cff4fcd62f5793c1a6e8abe5fda61f343ee513
GodGnidoc <godgnidoc@gmail.com>
2025-04-10 16:20:43 +0000

    Implement multiple code changes to enhance functionality and improve performance

--- play/include/syntax.h
+++ play/include/syntax.h
@@ -7,168 +7,171 @@ namespace play {
 
 struct BranchNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs branchs{};
+    
+    
     
 };
 struct ControlNode: public alioth::ASTNtrmNode {
+    struct BreakControl;
+    struct ContinueControl;
+    struct ReturnControl;
     
-    // not formed
+
+    
     
 };
+struct ControlNode::BreakControl {
+    ASTAttrs action{};
+    
+    
+};
+struct ControlNode::ContinueControl {
+    ASTAttrs action{};
+    
+    
+};
+struct ControlNode::ReturnControl {
+    ASTAttrs action{};
+    
+    ASTAttrs expr{};
+    
+    
+};
+
+
 struct DeclareNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs init{};
+    
+    ASTAttrs name{};
+    
+    
     
 };
 struct ElseNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs stmts{};
+    
+    
     
 };
 struct ElseifNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs condition{};
+    
+    ASTAttrs stmts{};
+    
+    
     
 };
 struct ExpressionNode: public alioth::ASTNtrmNode {
-    struct AddExpression;
-    struct AndExpression;
-    struct AssignExpression;
-    struct BitandExpression;
-    struct BitnotExpression;
-    struct BitorExpression;
-    struct BitxorExpression;
-    struct BooleanExpression;
-    struct DivExpression;
-    struct MolExpression;
-    struct MulExpression;
-    struct NegExpression;
-    struct NotExpression;
-    struct NullExpression;
-    struct NumberExpression;
-    struct OrExpression;
-    struct StringExpression;
+    struct BinaryExpression;
+    struct MonoExpression;
     struct SubExpression;
-    struct SubexprExpression;
+    struct ValueExpression;
     struct VarExpression;
+    
+
     
     
 };
+struct ExpressionNode::BinaryExpression {
+    ASTAttrs lhs{};
+    
+    ASTAttrs op{};
+    
+    ASTAttrs rhs{};
+    
+    
+};
+struct ExpressionNode::MonoExpression {
+    ASTAttrs op{};
+    
+    ASTAttrs rhs{};
+    
+    
+};
+struct ExpressionNode::SubExpression {
+    ASTAttrs expr{};
+    
+    
+};
+struct ExpressionNode::ValueExpression {
+    ASTAttrs false{};
+    
+    ASTAttrs null{};
+    
+    ASTAttrs number{};
+    
+    ASTAttrs string{};
+    
+    ASTAttrs true{};
+    
+    
+};
+struct ExpressionNode::VarExpression {
+    ASTAttrs name{};
+    
+    
+};
+
+
 struct FunctionNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs name{};
+    
+    ASTAttrs params{};
+    
+    ASTAttrs stmts{};
+    
+    
     
 };
 struct IfNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs condition{};
+    
+    ASTAttrs stmts{};
+    
+    
     
 };
 struct IterateNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs cond{};
+    
+    ASTAttrs ctrl{};
+    
+    ASTAttrs init{};
+    
+    ASTAttrs stmts{};
+    
+    
     
 };
 struct PlayNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs stmts{};
     
-};
-struct Priority0Node: public alioth::ASTNtrmNode {
-    struct BitnotPriority0;
-    struct BooleanPriority0;
-    struct NegPriority0;
-    struct NotPriority0;
-    struct NullPriority0;
-    struct NumberPriority0;
-    struct StringPriority0;
-    struct SubexprPriority0;
-    struct VarPriority0;
-    
-    
-};
-struct Priority1Node: public alioth::ASTNtrmNode {
-    struct BitandPriority1;
-    struct BitnotPriority1;
-    struct BitorPriority1;
-    struct BitxorPriority1;
-    struct BooleanPriority1;
-    struct NegPriority1;
-    struct NotPriority1;
-    struct NullPriority1;
-    struct NumberPriority1;
-    struct StringPriority1;
-    struct SubexprPriority1;
-    struct VarPriority1;
-    
-    
-};
-struct Priority2Node: public alioth::ASTNtrmNode {
-    struct BitandPriority2;
-    struct BitnotPriority2;
-    struct BitorPriority2;
-    struct BitxorPriority2;
-    struct BooleanPriority2;
-    struct DivPriority2;
-    struct MolPriority2;
-    struct MulPriority2;
-    struct NegPriority2;
-    struct NotPriority2;
-    struct NullPriority2;
-    struct NumberPriority2;
-    struct StringPriority2;
-    struct SubexprPriority2;
-    struct VarPriority2;
-    
-    
-};
-struct Priority3Node: public alioth::ASTNtrmNode {
-    struct AddPriority3;
-    struct BitandPriority3;
-    struct BitnotPriority3;
-    struct BitorPriority3;
-    struct BitxorPriority3;
-    struct BooleanPriority3;
-    struct DivPriority3;
-    struct MolPriority3;
-    struct MulPriority3;
-    struct NegPriority3;
-    struct NotPriority3;
-    struct NullPriority3;
-    struct NumberPriority3;
-    struct StringPriority3;
-    struct SubPriority3;
-    struct SubexprPriority3;
-    struct VarPriority3;
-    
-    
-};
-struct Priority4Node: public alioth::ASTNtrmNode {
-    struct AddPriority4;
-    struct AndPriority4;
-    struct BitandPriority4;
-    struct BitnotPriority4;
-    struct BitorPriority4;
-    struct BitxorPriority4;
-    struct BooleanPriority4;
-    struct DivPriority4;
-    struct MolPriority4;
-    struct MulPriority4;
-    struct NegPriority4;
-    struct NotPriority4;
-    struct NullPriority4;
-    struct NumberPriority4;
-    struct OrPriority4;
-    struct StringPriority4;
-    struct SubPriority4;
-    struct SubexprPriority4;
-    struct VarPriority4;
     
     
 };
 struct StmtNode: public alioth::ASTNtrmNode {
     
-    // not formed
+    ASTAttrs branch{};
+    
+    ASTAttrs control{};
+    
+    ASTAttrs declare{};
+    
+    ASTAttrs expression{};
+    
+    ASTAttrs function{};
+    
+    ASTAttrs iterate{};
+    
+    
     
 };
 
